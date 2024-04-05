1cc8c2340229a7fbb26b000ea67310cf82aee6d8
Matt Johanson <matt.johanson6@gmail.com>
2024-04-05 14:14:24 -0500

    Add guessing game with user input

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from random import randint
 
-random_number = randint(1,10)
+random_number = randint(1, 10)
+
+user_guess = input("Guess a number between 1 and 10: ")
+
+if user_guess == random_number:
+    print("You Guessed the number!")
+else:
+    print("Nope that was not the number!")
+
